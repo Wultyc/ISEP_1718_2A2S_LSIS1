7f9b53256ce37dee4326e313be7563667668e919
Jorge Gabriel Azevedo <1160929@isep.ipp.pt>
2018-06-02 00:15:30 +0100

    parametrizacao arduino

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,28 +1,32 @@
 //Parametrizacao
-#define MOTOR_A_DIR;
-#define MOTOR_A_PWM;
-#define MOTOR_B_DIR;
-#define MOTOR_B_PWM;
+#define MOTOR_A_DIR = 12;
+#define MOTOR_B_DIR = 13;
+#define MOTOR_A_PWM = 3;
+#define MOTOR_B_PWM = 11;
+#define MOTOR_A_BRK = 9;
+#define MOTOR_B_BRK = 8;
+#define MOTOR_A_CS = "A0";
+#define MOTOR_B_CS = "A1";
 
-#define SONAR_TRIG;
-#define SONAR_ECHO_FRENTE;
-#define SONAR_ECHO_DIREITA;
-#define SONAR_ECHO_ESQUERDA;
+#define SONAR_TRIG = "A4";
+#define SONAR_ECHO_FRENTE = "A1";
+#define SONAR_ECHO_DIREITA = "A2";
+#define SONAR_ECHO_ESQUERDA = "A3";
 
-#define VENTOINHA_INA;
-#define VENTOINHA_INB;
+#define VENTOINHA_INA = 6;
+#define VENTOINHA_INB = 5;
 
-#define SERVO_MOTOR;
-#define SERVO_DELAY;
+#define SERVO_MOTOR = 9;
+#define SERVO_DELAY = 25;
 
-#define CHAMA_PIN;
-#define CHAMA_LED;
+#define CHAMA_PIN = "A5";
+#define CHAMA_LED = 10;
 
-#define BT_RX;
-#define BT_TX;
+#define BT_RX = 0;
+#define BT_TX = 1;
 
-#define BOTAO_VERDE;
-#define BOTAO_VERML;
+#define BOTAO_VERDE = 8;
+#define BOTAO_VERML = 7;
 
 
 void setup() {
