cdf96e78bd25fb1cfffd3a777ea23f37f51be977
alunos <1160929@isep.ipp.pt>
2018-06-05 09:59:17 +0100

    Mudanca de Pinos e ćodigo de estados

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -3,8 +3,8 @@
 #define MOTOR_B_DIR = 13;
 #define MOTOR_A_PWM = 3;
 #define MOTOR_B_PWM = 11;
-#define MOTOR_A_BRK = 9;
-#define MOTOR_B_BRK = 8;
+//#define MOTOR_A_BRK = 9;
+//#define MOTOR_B_BRK = 8;
 #define MOTOR_A_CS = "A0";
 #define MOTOR_B_CS = "A1";
 
@@ -12,6 +12,7 @@
 #define SONAR_ECHO_FRENTE = "A2";
 #define SONAR_ECHO_DIREITA = "A3";
 #define SONAR_ECHO_ESQUERDA = "A4";
+#define SONAR_DIST = 7;
 
 #define VENTOINHA_INA = 6;
 
@@ -21,12 +22,22 @@
 #define CHAMA_PIN = "A5";
 #define CHAMA_LED = 7;
 
-#define BT_RX = 0;
-#define BT_TX = 1;
+#define BT_RX = 8;
+#define BT_TX = 9;
 
 #define BOTAO_VERDE = 4;
 #define BOTAO_VERML = 5;
 
+int rActivate = 0;
+int rState = -1;
+
+int define_state(){
+  return 0;
+}
+
+bool send_state(int state){
+  return true;
+}
 
 void setup() {
   // put your setup code here, to run once:
@@ -34,6 +45,25 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  //Determina o estado adequado
+  rState = define_state();
 
+  //Envia ao Sistema de Informação
+  send_state(rState);
+
+  //Atua mediante o estado
+  switch(rState){
+    case 0: //Parado
+      break;
+    case 1: //Andar para a Frente
+      break;
+    case 2: //Andar para a Trás
+      break;
+    case 3: //Rodar para a direita
+      break;
+    case 4: //Rodar para a esquerda
+      break;
+    case 5: //Apagar a chama
+      break;
+  }
 }
